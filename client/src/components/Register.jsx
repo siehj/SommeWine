e5c1b1ac36ea1169d8fff2210a9319fd74d4ff29
Sieh Johnson <johnson_sieh@yahoo.com>
2018-12-18 17:49:57 -0600

    Implement routing back to homepage for better UI/UX experience

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Link, History, BrowserRouter as Router } from 'react-router-dom';
 
 class Register extends React.Component {
   constructor(props) {
@@ -11,24 +12,25 @@ class Register extends React.Component {
 
   handleChange() {
     if(this.state.currScreen === 'Login') {
-      this.setState({currScreen: 'Sign Up'});
+      this.setState({ currScreen : 'Sign Up' });
     } else {
-      this.setState({currScreen: 'Login'});
+      this.setState({ currScreen : 'Login' });
     }
   }
 
   render() {
     return (
       <div id="register">
-        {this.state.currScreen === 'Login' ? (<h1>Login</h1>): (<h1>Sign Up</h1>)}
+        {this.state.currScreen === 'Login' ? <h1>Login</h1> : <h1>Sign Up</h1>}
         <form>
           Username: <input onChange={this.props.change} type="text" name="username"/>
           Password: <input onChange={this.props.change} type="password" name="password" />
           <input type="button" value={this.state.currScreen} onClick={this.props.register}/>
           <br/>
           <div>
-            I need to <a onClick={this.handleChange.bind(this)}>{ this.state.currScreen === 'Login' ? "Sign Up" : "Login"}</a>.
+            I need to <a onClick={this.handleChange.bind(this)}> { this.state.currScreen === 'Login' ? "Sign Up" : "Login"}</a>.
           </div>
+            <Link to="/" >Cancel</Link>
         </form>
       </div>
     )
