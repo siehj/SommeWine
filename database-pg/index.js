2607e98ae6e26a385160349c09aaef7e7db32ad4
Sieh Johnson <johnson_sieh@yahoo.com>
2018-12-19 13:52:32 -0600

    Establish successfull connection to Heroku pg database

--- database-pg/index.js
+++ database-pg/index.js
@@ -2,56 +2,92 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client(process.env.DATABASE_URL + '?ssl=true');
-client.connect();
+client.connect((err) => err ? console.log(err) : console.log('connected successfully!') );
 
-var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
-    }
+// var selectAll = function(callback) {
+//   connection.query('SELECT * FROM items', function(err, results) {
+//     if(err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, results);
+//     }
+//   });
+// };
+
+const addUser = (user) => {
+  let { username, password, name } = user;
+  const query = `INSERT into users (username, password, name, vote) VALUES ($1, $2, $3, 1);`;
+  const params = [username, password, name];
+  return new Promise ((resolve, reject) => {
+    client.query(query, params, (err, data) => {
+      if (err) reject(err);
+      else {
+        getUserInfo(username, (err, data) => {
+          if (err) reject(err);
+          else resolve(data);
+        })
+      };
+    });
   });
 };
 
-let getAllUserData = () => {
-  //will call on other functions. 
-}
-
-let addUser = (un, pw, callback) => {
-  connection.query('INSERT INTO users (username, password) VALUES ();');
-}
-
-let checkUser = (un, callback) => {
-  let query = `SELECT id FROM users WHERE EXISTS ()`;
-
-  //will also need to check the password.
-}
-
-
-let getUserFavorites = (userId, callback) => {
-  let query = `SELECT * FROM user_wines WHERE user_id=${userId};`;
-  connection.query(query, (err, results) => {
-
+const checkUsername = (username) => {
+  const query = 'SELECT EXISTS (SELECT 1 FROM users WHERE username=$1);';
+  const params = [username];
+  return new Promise ((resolve, reject) => {
+    client.query(query, params, (err, { rows }) => {
+      if (err) reject(err);
+      else resolve(rows[0].exists);
+    })
   });
-
 }
 
-let addUserFavorites = (userId, wine, callback) => {
-  let query = `INSERT INTO user_wines `
-}
+const findUser = (username) => {
+  const query = 'SELECT EXISTS (SELECT 1 FROM users WHERE username=$1);';
+  const params = [username];
+  return new Promise ((resolve, reject) => {
 
-let findWineId = (wineName, callback) => {
-  let query = `SELECT id FROM wines WHERE name=${wineName};`;
-  connection.query(query, (err, result) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, result);
-    }
+    client.query(query, params, (err) => {
+      if (err) reject(err);
+      getUserInfo(username, (err, data) => {
+        if (err) reject(err);
+        else resolve(data[0]);
+      })
+    });   
   })
+};
+
+getUserInfo = (username, callback) => {
+  const query = 'SELECT * FROM users WHERE username=$1;';
+  const params = [username];
+  client.query(query, params, (err, { rows }) => {
+    if(err) callback(err, null);
+    else callback(null, rows);
+  });
 }
+// let getUserFavorites = (userId, callback) => {
+//   let query = `SELECT * FROM user_wines WHERE user_id=${userId};`;
+//   connection.query(query, (err, results) => {
+
+//   });
+
+// }
+
+// let addUserFavorites = (userId, wine, callback) => {
+//   let query = `INSERT INTO user_wines `
+// }
+
+// let findWineId = (wineName, callback) => {
+//   let query = `SELECT id FROM wines WHERE name=${wineName};`;
+//   connection.query(query, (err, result) => {
+//     if (err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, result);
+//     }
+//   })
+// }
 
 
-module.exports = {selectAll};
+module.exports = { addUser, checkUsername, findUser, getUserInfo };
 
