c6dab8141a550daf8925c0c23882a47d1f56f6da
Sieh Johnson <johnson_sieh@yahoo.com>
2018-12-18 18:47:00 -0600

    Finish implementation of routes for login, signup and landing

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
-import { Route, Link, History, BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currScreen: 'Login',
-      
-    }
-  }
-
-  handleChange() {
-    if(this.state.currScreen === 'Login') {
-      this.setState({ currScreen : 'Sign Up' });
-    } else {
-      this.setState({ currScreen : 'Login' });
+      currScreen: this.props.location.pathname.slice(1),
+      error: ''
     }
   }
 
   render() {
     return (
       <div id="register">
-        {this.state.currScreen === 'Login' ? <h1>Login</h1> : <h1>Sign Up</h1>}
+        {this.state.currScreen === 'login' ? <h1>Login</h1> : <h1>Sign Up</h1>}
         <form>
           Username: <input onChange={this.props.change} type="text" name="username"/>
           Password: <input onChange={this.props.change} type="password" name="password" />
-          <input type="button" value={this.state.currScreen} onClick={this.props.register}/>
+          <input type="button" value={this.state.currScreen === 'login' ? 'Login' : 'Sign Up' }  onClick={this.props.register}/>
           <br/>
           <div>
-            I need to <a onClick={this.handleChange.bind(this)}> { this.state.currScreen === 'Login' ? "Sign Up" : "Login"}</a>.
+            I need to { this.state.currScreen === 'login' ? <Link to='/signup'>Sign Up</Link> : <Link to='/login'>Login</Link> }.
           </div>
             <Link to="/" >Cancel</Link>
         </form>
