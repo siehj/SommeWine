d2a95055b3aaeba4920055f3bd713f6a0b9537ea
Sieh Johnson <johnson_sieh@yahoo.com>
2018-12-20 09:11:53 -0600

    Implement express-session user saving feature

--- server/routes.js
+++ server/routes.js
@@ -2,9 +2,14 @@ let api = require('./routes/api');
 let db = require('./routes/db.js');
 let router = require('express').Router();
 
-// router.get('/', (req, res) => {
-
-// })
+router.get('/logout', (req, res) => {
+  res.redirect('/');
+})
 router.post('/db/register', db.register)
+
+router.get('/server/session', (req, res) => {
+  console.log(req.session)
+  res.end();
+})
 
 module.exports = router;
--- server/routes/db.js
+++ server/routes/db.js
@@ -1,66 +1,6 @@
 let db = require('../../database-pg/index.js');
 const bcrypt = require('bcrypt-nodejs');
 
-// let login = ({ username, password }) => { 
-
-//   if ( !username.length || !password.length ) {
-//     res.send("Either the username or password is missing");
-//   } 
-//   db.findUser(username)
-//     .then(user => {
-//       if(!user) {
-//         res.send("Sorry, your login and or password are incorrect!");
-//       } else {
-//         console.log(user);
-//         return user;
-//       }
-//     })
-//     .then(user => { 
-//       bcrypt.compare(password, user.password, (err, match) => {
-//         if (err || !match) {
-//           res.send(["login", "Sorry, your login and or password are incorrect!"]);
-//         } else {
-//           req.session.loggedIn = true;
-//           let userInfo = { id: user.id, name: user.name, email: user.email, phone: user.phone, vote: user.vote, city: user.city, state: user.state };
-//           res.send(userInfo);                  
-//         }
-//       })
-//     })
-//     .catch(err => console.log)
-//   };
-
-// let signup = ({ username, password }) => { 
-
-//   if ( !username.length || !password.length ) {
-//     console.log("error");
-//     cb("Either the username or password is missing", null);
-//   }
-
-//   bcrypt.genSalt(10, (err, salt) => {
-//     bcrypt.hash(password, salt, null, (err, hash) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         // check if username is taken in the db
-//         db.checkUsername(username)
-//           .then(result => {
-//             if (result) return cb("Sorry, this username is already taken");
-//             else {} 
-//             let user = { username: username, password: hash };
-//             db.addUser(user)
-//               .then(newUser => {
-//                 let userInfo = { username: newUser[0].username };
-//                 cb(userInfo);
-//               })
-//               .catch(err => console.log(err));
-//           })
-//           .catch(err => console.log(err));
-//       }
-//     });
-//   });
-
-//  };
-
 module.exports = {
   register: (req, res) => {
     let login = ({ username, password }) => { 
@@ -73,16 +13,17 @@ module.exports = {
           if(!user) {
             res.send("Sorry, your login and or password are incorrect!");
           } else {
-            // console.log(user);
             return user;
           }
         })
         .then(user => { 
           bcrypt.compare(password, user.password, (err, match) => {
             if (err || !match) {
-              res.send("Sorry, your login and or password are incorrect!");
+              res.send("Sorry, your username and or password are incorrect!");
             } else {
-              req.session.loggedIn = true;
+              req.session.user = { username: user.username, userId: user.id };
+              // console.log(req.session.user)
+              // console.log(req.session.id)
               let userInfo = { username: user.username };
               res.send(userInfo);                  
             }
@@ -108,10 +49,12 @@ module.exports = {
               .then(result => {
                 if (result) res.send("Sorry, this username is already taken");
                 else {
-                  let user = { username: username, password: hash };
+                  let user = { user: username, password: hash };
                   db.addUser(user)
                   .then(newUser => {
                     let userInfo = { username: newUser[0].username };
+                    req.session.user = { username: newUser[0].username, userId: newUser[0].id };
+                    // console.log(req.session.user)
                     res.send(userInfo);
                   })
                   .catch(err => console.log(err));
@@ -123,13 +66,14 @@ module.exports = {
       });
     
      };
-    
+     
     req.body.intent === 'login' ? 
-    login(req.body, () => res.send())
+    login(req.body)
     : 
     signup(req.body);
-
-
   },
+
+
+
 
 };
