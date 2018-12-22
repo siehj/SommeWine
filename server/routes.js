913ce59552080944f198b52d31ab12c367a11602
Sieh Johnson <johnson_sieh@yahoo.com>
2018-12-22 09:13:48 -0600

    Refactor auth check logic and serving of application for better security coverage

--- server/routes.js
+++ server/routes.js
@@ -4,14 +4,22 @@ const db = require('./routes/db.js');
 const api = require('./routes/api');
 const path = require('path');
 
-router.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
-    if (err) {
-      res.redirect('/');
-    }
-  })
-})
+const isAuthenticated = (req, res, next) => {
+  req.session.user ? next() : next();
+}
 
+router.get('/*', isAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => err ? res.redirect('/'): null)
+});
+
+router.get('/dashboard*', isAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => err ? res.redirect('/'): null)
+});
+
+router.post('/server/session', (req, res) => {
+  // console.log(req.params);
+  console.log(req.session);
+});
 // router.get('/dashboard/*', (req, res) => {
 //   req.redirect('/')
 // })
--- service/auth.js
+++ service/auth.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const authenticate = (req, res) => {
+  // console.log(req.session)
   // console.log(req.session.user.username);
   // console.log(req.query.user);
   req.session.user.username === req.query.user ? res.send(true) : res.send(false);
@@ -9,8 +10,10 @@ const authenticate = (req, res) => {
 const authCheck = () => {
   let url = window.location.href.split('/')
   let givenUser = url[url.length - 1];
-  if (givenUser === localStorage.getItem('auth')) return true;
-  else return axios.get(`/auth?user=${givenUser}`).then(({ data }) => {return data});
+  if (givenUser === sessionStorage.getItem('auth')) return true;
+  else axios.get(`/auth?user=${givenUser}`).then(({ data }) => {
+    return data
+  });
 };
 
 module.exports = { authenticate, authCheck };
