c72a83fb680efcbe1c5e45501e5fd8589c60745d
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-01 16:09:03 -0600

    Implement devided cellar screen to allow user to click from a multitude of features

--- client/src/components/DashComponents/Cellar.jsx
+++ client/src/components/DashComponents/Cellar.jsx
@@ -12,12 +12,16 @@ class Cellar extends React.Component {
   render() {
     return (
       <div>
-        <h2 className="tabTitle">Cellar</h2>
+        {/* <h2 className="tabTitle">Cellar</h2> */}
         <div className="cellarHome">
-          <div className="cellarLeft"><h3 className="text-center clText" >Favorites</h3></div>
+          <div className="cellarLeft"><h3 className="text-center" style={{ marginTop: '40%' }} >Favorites</h3></div>
           <div className="cellarRight">
-            <h3>A</h3>
-            <h3>B</h3>
+            <div className="rightTop" >
+              <h3 className="rtText" >Photo</h3>
+            </div>
+            <div className="rightBottom" >
+              <h3 className="rbText" >Taste Later</h3>
+            </div>
           </div>
         </div>
       </div>
