672cf0f516111cc14732fe5d9fd8bbb1443ae74b
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-16 16:33:58 -0600

    Implement a seperate Cellar home component to be able to change between features

--- client/src/components/DashComponents/Cellar.jsx
+++ client/src/components/DashComponents/Cellar.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import '../../../dist/ComponentCss/cellar.css';
+import CellarHome from './CellarComponents/CellarHome.jsx';
 
 class Cellar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      screen: 'Home',
+      AddPhoto: false,
+      ShowTasteLater: false,
     };
+    this.toggleFeature = this.toggleFeature.bind(this);
   }
 
+  toggleFeature(target) {
+    console.log(target, this.state[target])
+    this.setState({ [target] : !this.state[target] });
+  }
+    
   render() {
     return (
       <div>
         {/* <h2 className="tabTitle">Cellar</h2> */}
-        <div className="cellarHome">
-          <div className="cellarLeft"><h3 className="text-center" style={{ marginTop: '40%' }} >Favorites</h3></div>
-          <div className="cellarRight">
-            <div className="rightTop" >
-              <h3 className="rtText" >Photo</h3>
-            </div>
-            <div className="rightBottom" >
-              <h3 className="rbText" >Taste Later</h3>
-            </div>
-          </div>
-        </div>
+       <CellarHome addPhoto={this.state.AddPhoto} toggle={this.toggleFeature} tasteLater={this.state.ShowTasteLater} />        
       </div>
     )
   }
---
+++ client/src/components/DashComponents/CellarComponents/CellarHome.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import CellarPhoto from './CellarPhoto.jsx';
+import TasteLater from './CellarTasteLater.jsx';
+
+const CellarHome = (props) => {
+  return (
+    <div className="cellarHome">
+      <div className="cellarLeft"><h3 className="text-center" style={{ marginTop: '40%' }} >Favorites</h3></div>
+      <div className="cellarRight">
+        <div className="rightTop" >
+          {
+            props.addPhoto ? <CellarPhoto toggle={props.toggle} /> : <h3 onClick={() => props.toggle('AddPhoto')} className="rtText" >Photo</h3>
+          }
+          
+        </div>
+        <div className="rightBottom" >
+          {
+            props.TasteLater ? <TasteLater toggle={props.toggle}  /> : <h3 onClick={() => props.toggle('ShowTasteLater')} className="rbText" >Taste Later</h3>
+          }
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default CellarHome;
