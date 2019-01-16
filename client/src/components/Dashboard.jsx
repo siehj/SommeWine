530b26acd13c0a9ed1334adcda43b0ac1a3f2797
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-16 01:33:33 -0600

    Set default tab to Search while configuring.

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -10,7 +10,7 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      curr: 'Profile',
+      curr: 'Search',
       tabs: ['Search', 'Cellar', 'Profile'],
       showAdv: false,
       user: '',
