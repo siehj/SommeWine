b5c85bd57da98451a7366b4b4def0d455ab9bf73
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-16 02:29:08 -0600

    Alter api call to handle new advanced query search method.

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,28 +2,11 @@ const api = require('../../service/api.js');
 
 module.exports = {
   search : (req, res) => {
-    let request = [];
-    if (req.body.query.length !== 0) {
-      request.push(req.body.query)
-    }
-    // all = Object.values(req.body.additional);
-    // if(req.body.additional !== undefined) {
-    //   if (req.body.additional.types) {
-    //     request.push(req.body.additional.types[0]);
-    //   }
-    //   if (req.body.additional.regions) {
-    //     request.push(req.body.additional.regions[0]);
-    //   }
-    //   if (req.body.additional.notes) {
-    //     request.push(req.body.additional.notes[0]);
-    //   }
-    // }
-    api.wineApi({query: request.join(' ')}, (err, list) => {
+
+    api.wineApi({ query: req.body.query }, (err, list) => {
       if (err) console.log(err);
       else res.send(list);
-    })
+    });
       
-    // api.wineApi(req.url)
-    // res.send()
   }
 };
