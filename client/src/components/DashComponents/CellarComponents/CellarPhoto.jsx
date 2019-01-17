e27318460f93d617106e6bca0c08edce70f8fb86
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-17 15:46:36 -0600

    Remove Cellar photo component until feature is implemented

--- client/src/components/DashComponents/CellarComponents/CellarPhoto.jsx
+++ client/src/components/DashComponents/CellarComponents/CellarPhoto.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import { Input, Button, Col, Row } from 'reactstrap';
 
-const CellarPhoto = (props) => {
-  return (
-    <div className="cellarPhoto" >
-      <h3 onClick={() => props.toggle('AddPhoto')} >COMING SOON</h3>
-      {/* <Col>
-        <Row className="text-center" >
-          <Input type="file" />
-        </Row>
-        <Row>
-          <Col>
-            <Button>Add to Favorites</Button>
-          </Col>
-          <Col>
-            <Button>Add to Taste List</Button>
-          </Col>
-        </Row>
-        <Button onClick={() => props.toggle('AddPhoto')} >Cancel</Button>
-      </Col> */}
-    </div>
-  )
-};
+const CellarPhoto = (props) => { return <h3 className="rtText" onClick={() => props.toggle('AddPhoto')} >COMING SOON</h3> };
 
 export default CellarPhoto;
 
+// return (
+//   <div className="cellarPhoto" >
+//   <Col>
+//     <Row className="text-center" >
+//       <Input type="file" />
+//     </Row>
+//     <Row>
+//       <Col>
+//         <Button>Add to Favorites</Button>
+//       </Col>
+//       <Col>
+//         <Button>Add to Taste List</Button>
+//       </Col>
+//     </Row>
+//     <Button onClick={() => props.toggle('AddPhoto')} >Cancel</Button>
+//   </Col>
+// </div>
+// )
