a5a91358ab3d7859adcacb1663221f6136286878
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-24 01:19:16 -0600

    Integrate the Taste later component for the user's Cellar

--- client/src/components/DashComponents/CellarComponents/CellarTasteLater.jsx
+++ client/src/components/DashComponents/CellarComponents/CellarTasteLater.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import CellarWine from './CellarWine.jsx';
+import { Col, Row, Button } from 'reactstrap';
 
 const TasteLater = (props) => {
   return (
-    <div className="tasteLater" >
-      <button onClick={() => props.toggle('ShowTasteLater')} >Cancel</button>
-    </div>
+    <Col >
+      <h2 className="tabTitle">Taste List</h2>
+      <Row >
+        <Col className="cellarFavorites" >
+        {
+          props.tasteList.map(wine => <CellarWine key={wine.id} wine={wine} />)
+        }
+        </Col>
+      </Row>
+      <div>
+        <Button onClick={() => props.toggle('ShowTasteLater')} outline color="secondary" block >Back</Button>
+      </div>
+    </Col>
   )
 }
 
