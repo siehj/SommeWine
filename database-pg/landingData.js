7e944c0863380399a379a54c2a62b00d819cc695
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-24 02:27:38 -0600

    Create landing data to import into the landing

---
+++ database-pg/landingData.js
@@ -0,0 +1,36 @@
+
+let LandingData = {
+  'SommeWine' : {
+    type: 1,
+    text: 'Your personal Sommelier, SommeWine helps you to keep track of the wines you love and save the ones you want to taste.'
+  },
+  'Customize Search' : {
+    type: 2,
+    img: '',
+    text: 'Customized search feature integrates with a wine API with thousands of wines from regions around the world.'
+  },
+  'Get Suggestions' : {
+    type: 2,
+    img: '',
+    text: 'Get suggestions based on numerous flavor palette information that you save.'
+  },
+  'Save Wines' : {
+    type: 3, 
+    img1: '',
+    img2: '',
+    text1: '',
+    text2: '',
+    col1: 'Favorite',
+    col2: 'Taste List'
+  },
+  'Coming Soon' : {
+    type: 4, 
+    bullets: ['Uploading photos of labels', 'Adding user comments to wines', 'Mobile Application']
+  },
+  'App Details' : {
+    type: 4, 
+    bullets: ['React', 'Reactstrap', 'ReactRouter', 'Node', 'PostgreSQL']
+  }
+}
+
+module.exports = { LandingData }
